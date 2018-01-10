02c61c2ccf056638078368b8fc1ef90c1213b977
rajivagain <rajivagain>
2018-01-10 15:28:02 +0545

    sdfsdf

---
+++ src/main/java/com/example/newp/HelloController.java
@@ -0,0 +1,13 @@
+package com.example.newp;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/")
+    public String index(){
+        return "index";
+    }
+}
---
+++ src/main/java/com/example/newp/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.example.newp;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(NewpApplication.class);
+	}
+
+}
